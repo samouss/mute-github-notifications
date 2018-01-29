3da3ee98083c34b066cdbec9b6eac110890260a3
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-29 18:58:23 +0100

    feat: add extension

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,12 @@
-console.log('contentScript');
+console.info('[GitHub]: mute notifications 🔇');
+
+const style = document.createElement('style');
+
+style.type = 'text/css';
+style.appendChild(document.createTextNode(`
+  .notification-indicator .mail-status.unread {
+    display: none !important;
+  }
+`));
+
+document.documentElement.appendChild(style);
